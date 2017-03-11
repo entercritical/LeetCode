61e4d356242ce06d3f65d48d56b64114de0a5d82
Suhwan Hwang <janush@naver.com>
2017-03-11 13:43:08 +0900

    Update 018_4Sum.cpp

--- 018_4Sum.cpp
+++ 018_4Sum.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> fourSum_using_map(vector<int>& nums, int target) {
         set<vector<int>> ans_set;
         multimap<int, pair<int, int>> sum_map;
 
@@ -41,6 +41,46 @@ public:
 
         vector<vector<int>> ans(ans_set.size());
         copy(ans_set.begin(), ans_set.end(), ans.begin());
+        return ans;
+    }
+    
+    // https://en.wikipedia.org/wiki/3SUM
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        if (nums.size() < 4) {
+            return ans;
+        }
+        sort(nums.begin(), nums.end()); // sorting
+        for (int first = 0; first < nums.size() - 3; ++first) {
+            if (first > 0 && nums[first - 1] == nums[first]) {
+				// skip same number
+                continue;
+            }
+            for (int second = first + 1; second < nums.size() - 2; ++second) {
+                if (second > first + 1 && nums[second - 1] == nums[second]) {
+					// skip same number
+                    continue;
+                }
+                int t = target - (nums[first] + nums[second]);
+                int low = second + 1;
+                int high = nums.size() - 1;
+                while (low < high) {
+                    int sum = nums[low] + nums[high];
+                    if (sum == t) {
+                        ans.push_back(vector<int>{nums[first], nums[second], nums[low], nums[high]});
+                        low++;
+                        high--;
+                        // skip same number
+                        while (nums[low - 1] == nums[low]) low++;
+                        while (nums[high + 1] == nums[high]) high--;
+                    } else if (sum > t) {
+                        high--; // decrease sum
+                    } else {
+                        low++; // increase sum
+                    }
+                }
+            }
+        }
         return ans;
     }
 };
