cbf5d2dee9fc1014775e59af88b113db0aeac676
Suhwan Hwang <janush@naver.com>
2017-03-11 10:52:34 +0900

    Update 500_Keyboard_Row.cpp

--- 500_Keyboard_Row.cpp
+++ 500_Keyboard_Row.cpp
@@ -7,11 +7,7 @@ using namespace std;
 
 class Solution {
 private:
-    unordered_map<char, int> keyboard = {
-        {'q', 1},{ 'w', 1 },{ 'e', 1 },{ 'r', 1 },{ 't', 1 },{ 'y', 1 },{ 'u', 1 },{ 'i', 1 },{ 'o', 1 },{ 'p', 1 },
-        { 'a', 2 },{ 's', 2 },{ 'd', 2 },{ 'f', 2 },{ 'g', 2 },{ 'h', 2 },{ 'j', 2 },{ 'k', 2 },{ 'l', 2 },
-        { 'z', 3 },{ 'x', 3 },{ 'c', 3 },{ 'v', 3 },{ 'b', 3 },{ 'n', 3 },{ 'm', 3 }
-    };
+    unordered_map<char, int> keyboard;
     bool isOneRow(string s) {
         int row = 0;
         for (int i = 0; i < s.length(); ++i) {
@@ -24,6 +20,14 @@ private:
         return true;
     }
 public:
+    Solution() {
+        vector<string> k{ "qwertyuiop", "asdfghjkl", "zxcvbnm" };
+        for (int row = 0; row < k.size(); ++row) {
+            for (int i = 0; i < k[row].length(); ++i) {
+                keyboard.insert(make_pair(k[row][i], row + 1));
+            }
+        }
+    }
     vector<string> findWords(vector<string>& words) {
         vector<string> ans;
         for (string s : words) {
